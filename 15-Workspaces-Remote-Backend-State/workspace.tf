9f1a7ada5091603b5e4e2c366021fc01eaa9b2a9
root <root@your.domain.com>
2020-07-08 09:29:45 +0000

    15-Workspaces-Remote-Backend-State/workspace.tf

---
+++ 15-Workspaces-Remote-Backend-State/workspace.tf
@@ -0,0 +1,37 @@
+#########GCP##########
+terraform {
+  backend "gcs" {
+    bucket  = "amitvashist7-terraform-state"
+    prefix  = "terraform/user/tf01/state/workspaces/"
+    credentials = "/tmp/account.json"
+  }
+}
+
+provider "google" {
+  project     = "gleaming-design-282503"
+  credentials = "${file("/tmp/account.json")}"
+  region      = "us-west1"
+  zone        = "us-west1-c"
+}
+
+locals {
+  default_name = "${join("-", list(terraform.workspace, "example"))}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "${local.default_name}"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
