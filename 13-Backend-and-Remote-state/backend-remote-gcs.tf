4110f504d8bbc225f7d49049ba442fb809bed463
Amit <amitvashist.tech@gmail.com>
2020-07-08 06:26:40 +0000

    12-Modules

--- 13-Backend-and-Remote-state/backend-remote-gcs.tf
+++ 13-Backend-and-Remote-state/backend-remote-gcs.tf
@@ -2,20 +2,29 @@
 terraform {
   backend "gcs" {
     bucket  = "amitvashist7-terraform-state"
-    prefix  = "terraform/user/tf7/state"
-    credentials = "/home/terrafrom07/.ssh/account.json"
+    prefix  = "terraform/user/tf01/state"
+    credentials = "/tmp/account.json"
   }
 }
 
+
+######## Remote TFState with Local Backend - Remote DIR ########
+#terraform {
+#   backend "local" {
+#     path = "/tmp/terraform/localbackend/remote-state/demo/terraform.tfstate" 
+#  }
+#}
+
+
 provider "google" {
-  credentials = "${file("/home/terrafrom07/.ssh/account.json")}"
-  project     = "k8s-terraform-demo-272708"
+  credentials = "${file("/tmp/account.json")}"
+  project     = "gleaming-design-282503"
   region      = "us-west1"
   zone        = "us-west1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance-1"
+  name         = "terraform-gcs-vm-2"
   machine_type = "f1-micro"
 
   boot_disk {
